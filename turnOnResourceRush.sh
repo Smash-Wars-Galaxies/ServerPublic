5e8b15b2bd8fbbd8dbdadd7b36dcb8e9d1f360a6
InfiniteAxis <InfiniteAxis@users.noreply.github.com>
2024-10-14 19:17:16 -0400

    Add Scripts to Manage ResourceRush

---
+++ turnOnResourceRush.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/SamplingMultiplier?value=2.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/MilkMultiplier?value=4.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/ForageMultiplier?value=3.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/FishingMultiplier?value=4.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/HarvestMultiplier?value=1.5" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
