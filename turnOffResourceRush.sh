1defae20c1923c3e1642d8cf7f0a7e51d1cb82f2
InfiniteAxis <InfiniteAxis@users.noreply.github.com>
2024-10-14 22:20:14 -0400

    Fix URLs

--- turnOffResourceRush.sh
+++ turnOffResourceRush.sh
@@ -2,6 +2,6 @@
 
 http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/SamplingMultiplier?value=1.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
 http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/MilkMultiplier?value=1.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
-http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/ForagingMultiplier?value=1.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/ForageMultiplier?value=1.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
 http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/FishingMultiplier?value=1.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
-http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/HarvestingMultiplier?value=1.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/HarvestMultiplier?value=1.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
