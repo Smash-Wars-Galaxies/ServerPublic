bc0c6ae71c0d96e34d5d1d8dfdf556ca27b816bd
InfiniteAxis <InfiniteAxis@users.noreply.github.com>
2024-10-14 22:17:21 -0400

    Fix for HTTPie

--- turnOnResourceRush.sh
+++ turnOnResourceRush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-http PUT -A bearer -a VJaSqx0UyJb3AnmcU0AU+x3a "http://localhost:44443/v1/admin/config/Core3/Resource/SamplingMultiplier?value=2.0"
-http PUT -A bearer -a VJaSqx0UyJb3AnmcU0AU+x3a "http://localhost:44443/v1/admin/config/Core3/Resource/FishingMultiplier?value=4.0"
-http PUT -A bearer -a VJaSqx0UyJb3AnmcU0AU+x3a "http://localhost:44443/v1/admin/config/Core3/Resource/ForagingMultiplier?value=3.0"
-http PUT -A bearer -a VJaSqx0UyJb3AnmcU0AU+x3a "http://localhost:44443/v1/admin/config/Core3/Resource/MilkMultiplier?value=4.0"
-http PUT -A bearer -a VJaSqx0UyJb3AnmcU0AU+x3a "http://localhost:44443/v1/admin/config/Core3/Resource/HarvestMultiplier?value=1.5"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/SamplingMultiplier?value=2.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/MilkMultiplier?value=4.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/ForagingMultiplier?value=3.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/FishingMultiplier?value=4.0" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
+http PUT "http://localhost:44443/v1/admin/config/Core3/Resource/HarvestingMultiplier?value=1.5" "Authorization:Bearer VJaSqx0UyJb3AnmcU0AU+x3a"
