64f9824f4bec25f885900106337e077193ea4c4c
Omegaice <950526+Omegaice@users.noreply.github.com>
2024-10-18 16:16:47 -0400

    feat(resources): added config options to allow for scaling sampling, harvesting, fishing, foraging and milking

--- MMOCoreORB/src/server/zone/managers/minigames/ForageManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/minigames/ForageManagerImplementation.cpp
@@ -401,8 +401,8 @@ bool ForageManagerImplementation::forageGiveResource(TransactionLog& trx, Creatu
 			return false;
 		}
 	}
-
-	int quantity = System::random(30) + 10;
+	const float resourceMultiplier = ConfigManager::instance()->getFloat("Core3.Resource.ForageMultiplier", 1.0);
+	int quantity = System::random(30) + 10 * resourceMultiplier;
 	resourceManager->harvestResourceToPlayer(trx, player, resource, quantity);
 	return true;
 }
