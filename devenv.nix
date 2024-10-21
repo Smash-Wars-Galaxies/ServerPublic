78a387140b12899e9c993f7ec4d3c5b2d50aab92
Omegaice <950526+Omegaice@users.noreply.github.com>
2024-10-21 15:35:48 -0400

    feat(actions): initial github-action to build with devenv

--- devenv.nix
+++ devenv.nix
@@ -22,46 +22,41 @@ in {
     pkgs.zlib.dev
     pkgs.gtest
     pkgs.libgcc.lib
+    pkgs.gdb
     cpprest
+    pkgs.ccache
   ];
 
   languages.cplusplus.enable = true;
-
-  tasks = {
-    "swgemu:clean" = {
-      exec = ''
-        [ -d "$PWD/MMOCoreORB/compile" ] && rm -rf $PWD/MMOCoreORB/compile
-      '';
-    };
-    "swgemu:configure" = {
-      exec = ''
-        [ ! -d "$PWD/MMOCoreORB/compile" ] && mkdir $PWD/MMOCoreORB/compile
-        cd $PWD/MMOCoreORB/compile
-        ${pkgs.cmake}/bin/cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_REST_SERVER=ON ..
-      '';
-    };
-    "swgemu:build" = {
-      exec = ''
-        cd $PWD/MMOCoreORB/compile
-        ${pkgs.cmake}/bin/cmake --build . --parallel 12 --target all --
-      '';
-      after = ["swgemu:configure"];
-    };
-  };
+  
+  dotenv.enable = true;
 
   env = {
     hardeningDisable = ["all"];
+    CCACHE_DIR = "${config.devenv.root}/state/ccache";
+    PROJECT_ROOT = "${config.devenv.root}/MMOCoreORB";
+    PROJECT_BUILD = "${config.devenv.root}/MMOCoreORB/compile";
   };
 
-  # https://devenv.sh/processes/
-  processes = {
-    server = {
-      exec = "./core3";
+  scripts = {
+    "project-update-tre".exec = ''
+      ${lib.getExe pkgs.minio-client} alias set smashwg https://s3.hellafast.io $SMASHWG_S3_ACCESS $SMASHWG_S3_SECRET
+      ${lib.getExe pkgs.minio-client} mirror --overwrite "smashwg/tre-files/tre/" "$PROJECT_ROOT/bin/tre/"
+    '';
+    "project-configure".exec = ''
+      ${lib.getExe pkgs.cmake} -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_REST_SERVER=ON -S "$PROJECT_ROOT" -B "$PROJECT_BUILD"
+    '';
+    "project-build".exec = ''
+      ${lib.getExe pkgs.cmake} --build "$PROJECT_BUILD" --parallel $(${pkgs.coreutils}/bin/nproc) --target all --
+    '';
+  };
 
-      process-compose = {
-        is_tty = true;
-        working_dir = "./MMOCoreORB/bin";
-      };
+  tasks = {
+    "swgemu:clean".exec = ''[ -d "$PROJECT_BUILD" ] && rm -rf "$PROJECT_BUILD"'';
+    "swgemu:configure".exec = ''project-configure'';
+    "swgemu:build" = {
+      exec = ''project-build'';
+      after = ["swgemu:configure"];
     };
   };
 
@@ -84,4 +79,17 @@ in {
       }
     ];
   };
+
+  enterTest = ''
+    project-update-tre
+
+    wait_for_port 3306 # Wait for MySQL
+    while ! "${pkgs.mariadb}/bin/mysql" -h"localhost" -u"swgemu" -p"123456" -e "USE swgemu;" 2>/dev/null; do
+      sleep 1
+    done
+
+    cd MMOCoreORB/bin
+    cp conf/config.lua.example conf/config.lua && cat conf/config.lua
+    ./core3 runUnitTests --gtest_output=xml:core3_tests.xml
+  '';
 }
