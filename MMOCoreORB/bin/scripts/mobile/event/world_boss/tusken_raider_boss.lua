df93dfeb99c4051773d701da9628d14dd4523a5d
Omegaice <950526+Omegaice@users.noreply.github.com>
2024-10-23 14:31:07 -0400

    fix(mob): tusken raider boss mob primary attacks

--- MMOCoreORB/bin/scripts/mobile/event/world_boss/tusken_raider_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/event/world_boss/tusken_raider_boss.lua
@@ -70,7 +70,7 @@ tusken_raider_boss = Creature:new {
 	
 	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
 	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
-	primaryAttacks = merge(marksmanmaster,brawlermaster,fencermaster,riflemanmaster, { {"poisongascloud",""} }),
+	primaryAttacks = merge(marksmanmaster,brawlermaster,fencermaster,riflemanmaster),
 	secondaryAttacks = { }
 }
 
